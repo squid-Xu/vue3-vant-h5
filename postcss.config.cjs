39ffffc38faafee7fcecd91bcd491aadb78f6333
徐梦强 <1135349080@qq.com>
2024-09-04 16:20:29 +0800

    rem布局适配

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,10 +3,9 @@ module.exports = {
       // postcss-pxtorem 插件的版本需要 >= 5.0.0
       'postcss-pxtorem': {
         rootValue({ file }) {
-          return file.indexOf('vant') !== -1 ? 37.5 : 75; // 假设设计稿宽度750px
+          return file.indexOf('.vant') !== -1 ? 37.5 : 75; // 假设设计稿宽度750px
         },
         propList: ['*'],
       },
     },
   };
-  
