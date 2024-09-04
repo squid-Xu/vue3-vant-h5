0af4fe9d9ee5e7e5a1dbafc21b4842fe593654de
徐梦强 <1135349080@qq.com>
2024-09-04 16:28:01 +0800

    路径别名配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from '@vant/auto-import-resolver';
 
+// 引入path模块
+import path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -16,4 +19,10 @@ export default defineConfig({
       resolvers: [VantResolver()],
     }),
   ],
+  //路径别名
+  resolve: {
+    alias: {
+        '@': path.resolve(__dirname, 'src')
+    }
+  },
 })
